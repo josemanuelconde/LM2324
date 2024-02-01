c6a98a1c8522e56e10e48e82dd3611a04ffb4538
josemanuelconde <josemanuelconde03@iesamachado.org>
2024-02-01 14:17:50 +0100

    a

---
+++ 2º Trimestre/formulario.js
@@ -0,0 +1,19 @@
+function validar(elementos)
+{
+    let estanCorrectos = true
+        
+   for(var i=0;i<elementos.length; i++) 
+   {
+
+    if(elementos[i].value == "")
+    {
+        
+        estanCorrectos = false
+    }
+
+   }
+    
+    estanCorrectos = false
+
+    return estanCorrectos;
+}
