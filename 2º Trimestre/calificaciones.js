92b549bc7bf2dcba08b1b2d52f3b8f0787dc1f62
josemanuelconde <josemanuelconde03@iesamachado.org>
2024-02-08 14:10:33 +0100

    califica

---
+++ 2º Trimestre/calificaciones.js
@@ -0,0 +1,44 @@
+var notas =[];
+
+function anadir()
+{
+    let numero = parseFloat(document.getElementById("numero").value);
+
+    document.getElementById("aviso").innerHTML ="";
+
+    if(isNaN(numero))
+    {
+           
+         
+        document.getElementById("aviso").innerHTML = "Debes introducir un número entre 0 y 10"
+    }
+        else
+    {
+        if(numero<0 || numero>10)
+        {
+            document.getElementById("aviso").innerHTML = "Debes introducir un número entre 0 y 10"
+        }
+        else
+        {
+            notas.push(numero);
+            document.getElementById("notas").innerHTML = notas;
+        }
+    }
+}
+
+function calcularMedia()
+{
+    let total =0.0;
+    for(var i=0; i<notas.length;i++)
+    {
+        total += notas[i];
+
+    }
+    total = total/notas.length;
+    document.getElementById("resultado").innerHTML = total.toFixed(2);
+}
+
+function borrar()
+{
+
+}
